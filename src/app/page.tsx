3df282de3fbfff0b5e028a83e91075a9e0087618
gauri Sonawane <gaurisonawane918@gmail.com>
2025-10-01 00:44:06 +0530

    changes to the home page before pulling updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Hero from "@/components/Hero/Hero";
-
+import BestOfOurCreations from "@/components/BestOfOurCreations";
+import HomeOwnersFeels from "@/components/HomeOwnersFeels";
+import InspirationToElevateLiving from "@/components/InspirationToElevateLiving";
+import TrustedPartners from "@/components/TrustedPartners";
 export default function Home() {
   return (
     <div className="min-h-screen bg-[var(--background)]">
       <Hero />
+      <InspirationToElevateLiving />
+      <BestOfOurCreations/>
+      <HomeOwnersFeels/>
+      <TrustedPartners />
+      
     </div>
   );
 }
