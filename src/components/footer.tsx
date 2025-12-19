153cfbcc91e05328d9df9477fbb85c80f78fdf20
The RD Group Of Industries <therdgroup24@gmail.com>
2025-12-19 13:30:28 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -98,7 +98,7 @@
 //             <div className="space-y-4">
 //               <div className="flex text-[18px] items-center gap-3">
 //                 <svg width="18" height="18" viewBox="0 0 24 24" fill="white"><path d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 14C9.33 14 4 15.34 4 18V19C4 19.55 4.45 20 5 20H19C19.55 20 20 19.55 20 19V18C20 15.34 14.67 14 12 14Z"/></svg>
-//                 <span>Ankit Sandiliya</span>
+//                 <span>Ankit Sandilya</span>
 //               </div>
 //               <div className="flex text-[18px] items-center gap-3">
 //                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" className="flex-shrink-0">
@@ -336,7 +336,7 @@ const Footer: React.FC = () => {
                     fill="white"
                   />
                 </svg>
-                <span>Ankit Sandiliya</span>
+                <span>Ankit Sandilya</span>
               </motion.div>
               <motion.div className="flex text-[18px] items-center gap-3" variants={textVariants} custom={12}>
                 <svg
